e2436ab5da78d7ff6351cbb9c520a7987c2c39a0
Air-Sergej <rsv1907@yandex.ru>
2022-03-30 21:42:41 +0300

    Ошибка ломающая сборку (ошибка id в посте)

--- src/main/kotlin/ru/netology/wall/WallService.kt
+++ src/main/kotlin/ru/netology/wall/WallService.kt
@@ -1,14 +1,48 @@
 package ru.netology.wall
 
-import ru.netology.post.Post
+import ru.netology.post.*
 
-class WallService(val posts: Array<Post>) {
-    private var curId = 1
-    fun add(post: Post): Post{
+class WallService(var posts: Array<Post>) {
+    private var curId = 0
+    fun add(post: Post): Post {
         val mutablePosts = posts.toMutableList()
         val newPost = post.copy(id = curId)
+        curId++
         mutablePosts.add(newPost)
         posts = mutablePosts.toTypedArray()
         return newPost
+    }
+
+    fun update(post: Post): Boolean {
+        val index = posts.indexOfFirst { p -> p.id == post.id }
+        if (index != -1) {
+            posts[index] = posts[index].copy(
+                id = curId,
+                fromId = post.fromId,
+                createdBy = post.createdBy,
+                text = post.text,
+                replyOwnerId = post.replyOwnerId,
+                replyPostId = post.replyPostId,
+                friendsOnly = post.friendsOnly,
+                comments = post.comments,
+                copyright = post.copyright,
+                likes = post.likes,
+                reposts = post.reposts,
+                views = post.views,
+                postType = post.postType,
+                signerId = post.signerId,
+                canPin = post.canPin,
+                canDelete = post.canDelete,
+                canEdit = post.canEdit,
+                isPinned = post.isPinned,
+                markedAsAds = post.markedAsAds,
+                isFavorite = post.isFavorite,
+                donut = post.donut,
+                postponedId = post.postponedId
+            )
+            return true
+        } else {
+            return false
+        }
     }
 }
